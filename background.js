74f470c2378d11a2c5638aeed133da076be05f13
leejooho388 <leejooho388@gmail.com>
2019-04-22 21:09:31 -0700

    adjusted icons and started implemention with commands set to alt+shift+ F/G"

--- background.js
+++ background.js
@@ -1,3 +1,29 @@
-chrome.runtime.onInstalled.addListener(function() {
+// chrome.runtime.onInstalled.addListener(function() {
   
+// });
+
+// chrome.runtime.onMessage.addListener(function(message, callback) {
+//   if (message == 'hello') {
+//     sendResponse({greeting: 'welcome!'})
+//   } else if (message == 'goodbye') {
+//     chrome.runtime.Port.disconnect();
+//   }
+  
+//   (message.data === 'runLogic') {
+//     chrome.tabs.executeScript({file: 'logic.js'});
+//   }
+// });
+
+chrome.commands.onCommand.addListener(function(command) {
+  if(command === "save-session"){
+    chrome.storage.sync.set({
+      session: []
+    }, () => {
+      console.log('saved');
+    })
+  } else if (command === "load-session"){
+   chrome.storage.sync.get('session', (session) => {
+     console.log('sessions ', session)
+   }) 
+  }
 });
